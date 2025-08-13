f881c9a02a824583bfa070d58621ac15e5ab6de1
DenisSavostenko0858 <denyotube0858@gmail.com>
2025-08-13 15:46:31 +0300

    Client: Added: - Новый элемент containers

---
+++ client/src/components/homeInfoContainers.tsx
@@ -0,0 +1,17 @@
+import { motion } from 'framer-motion';
+import ContainerRecipient from './containers/container-recipient';
+
+
+const HomeContainers = () => {
+  return (
+    <motion.div 
+    className="home-containers"
+    initial={{ opacity: 0, y: 20 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.5 }}>
+    
+    </motion.div>
+  );
+};
+
+export default HomeContainers;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import DrawerAppBar from '../components/header'
 import ExchangeComponent from '../components/exchanger'
+import ContainerHead from '../components/containers/container-text-head'
 import '../App.css'
 
 function App() {
   return (
     <>
     <DrawerAppBar />
+    <ContainerHead />
     <ExchangeComponent />
     </>
   )
